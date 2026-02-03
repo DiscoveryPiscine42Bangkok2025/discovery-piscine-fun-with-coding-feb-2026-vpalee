7b415311c67ba805198bfe9b2a195873b76e3bc1
vipalee <67070281@kmitl.ac.th>
2026-02-03 14:53:57 +0700

    Add files via upload

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Check if any arguments were provided
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    # Loop through all arguments provided to the script
+    for arg in "$@"
+    do
+        # Create a directory named "ex" followed by the argument
+        mkdir "ex$arg"
+    done
+fi
